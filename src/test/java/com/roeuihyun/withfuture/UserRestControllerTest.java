b373bba1cfe1657161e6c112187fcab678aa0bb3
roeuihyun <roeuihyun@gmail.com>
2022-08-04 23:58:10 +0900

    bom 추가 통합 테스트 -> 단위테스트로 변경

--- src/test/java/com/roeuihyun/withfuture/UserRestControllerTest.java
+++ src/test/java/com/roeuihyun/withfuture/UserRestControllerTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -31,9 +31,11 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @EnableAutoConfiguration
-@SpringBootTest(classes= {UserRestController.class}) // 테스트 대상 클래스 등록 
+//@SpringBootTest(classes= {UserRestController.class}) // 테스트 대상 클래스 등록 
 //@RunWith(SpringRunner.class)
-@ExtendWith(SpringExtension.class)	
+//@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserRestController.class)
 @AutoConfigureMockMvc
 @WebAppConfiguration
 public class UserRestControllerTest {
@@ -68,11 +70,9 @@ public class UserRestControllerTest {
         mockMvc.perform(get(BASE_URL).params(paramMap)
 //                .accept(MediaTypes.HAL_JSON_VALUE)
                 )
+        		.andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(status().isOk());
-//		.andExpect(status().isOk())
-//		.andExpect((ResultMatcher) content().string("hello"))
-		
+                ;
 	}
 
 }
