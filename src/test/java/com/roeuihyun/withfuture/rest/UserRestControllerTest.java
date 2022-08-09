4049e7159150c1d87695bad4e49a4b0b57362396
roeuihyun <roeuihyun@gmail.com>
2022-08-09 15:49:34 +0900

    junit 5 로 변경을 위해 auto wired로 변경

--- src/test/java/com/roeuihyun/withfuture/rest/UserRestControllerTest.java
+++ src/test/java/com/roeuihyun/withfuture/rest/UserRestControllerTest.java
@@ -11,16 +11,9 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
@@ -30,18 +23,17 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.roeuihyun.withfuture.entity.UserEO;
-import com.roeuihyun.withfuture.service.UserService;
-import com.roeuihyun.withfuture.store.UserStore;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@EnableAutoConfiguration
-//@ExtendWith(SpringExtension.class) 5 버전
-@RunWith(SpringRunner.class)
-//@SpringBootTest(classes= {UserRestController.class}) // 테스트 대상 클래스 등록
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@WebMvcTest(UserRestController.class)
-@AutoConfigureMockMvc
-@WebAppConfiguration
+//@EnableAutoConfiguration
+//@ExtendWith({SpringExtension.class}) //5 버전
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes= {StdWordController.class}) // 테스트 대상 클래스 등록
+//@WebMvcTest(StdWordController.class)
+//@AutoConfigureMockMvc
+//@WebAppConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@ActiveProfiles("local")
 public class UserRestControllerTest {
 	
     private final String BASE_URL = "/Users";
@@ -49,17 +41,12 @@ public class UserRestControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
-    
-    @MockBean
-    UserService userService;
-
-    @MockBean
-    UserStore userStore;
 
     @Autowired
-    WebApplicationContext ctx;
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext ctx;
     
     @BeforeEach
     public void setup() {
@@ -124,5 +111,125 @@ public class UserRestControllerTest {
                 .andDo(print())
                 .andExpect(status().isNotFound());
     }
+
+//    @Order(1)
+//    @DisplayName("표준 단어 전체 조회")
+//	@Test
+//	void getAllStdWord() throws Exception {
+//    	MultiValueMap<String, String> paramMap =new LinkedMultiValueMap<>();
+//    	paramMap.add("pageNum", "1");
+//    	paramMap.add("pageSize", "10");
+//        mockMvc.perform(
+//        		get(BASE_URL)
+//        		.characterEncoding("UTF-8")
+//                .contentType(MediaTypes.HAL_JSON_VALUE)
+//        		.params(paramMap)
+//        		)
+//				.andDo(print())
+//				;
+//		
+//	}
+//	
+//    @Order(2)
+//    @DisplayName("표준 단어 추가")
+//    @Transactional
+//	@Test
+//	void insertStdWord() throws Exception {
+//    	StdWordEO stdWordEO =  new StdWordEO();
+//    	stdWordEO.setWord_uuid(word_uuid);
+//    	stdWordEO.setWord_dictn_uuid(termn_dictn_uuid);
+//    	stdWordEO.setWord_nm("가동");
+//    	stdWordEO.setWord_eng_nm("Operating");
+//    	stdWordEO.setWord_abbr_nm("OPRTNG");
+//    	stdWordEO.setWord_expl("稼動. 사람이나 기계 따위가 움직여 일함. 또는 기계 따위를 움직여 일하게 함");
+//    	stdWordEO.setWord_clsf_cd("modifier");
+//    	stdWordEO.setFrst_crt_dt("20220704000000");
+//    	stdWordEO.setFrst_crtr("system");
+//    	stdWordEO.setLast_mdfcn_dt("20220704000000");
+//    	stdWordEO.setLast_mdfr("system");
+//    	stdWordEO.setDmnd_dt("20220704000000");
+//    	stdWordEO.setDel_yn(null);
+//		HashMap<String,Object> param = new HashMap<String,Object>();
+//		param.put("word_uuid", stdWordEO.getWord_uuid());
+//		param.put("stdWordEO", stdWordEO);
+//		
+//		mockMvc.perform(
+//				post(BASE_URL)
+//				.characterEncoding("UTF-8")
+//				.contentType(MediaTypes.HAL_JSON_VALUE)
+//				.content(objectMapper.writeValueAsString(param))
+//			)
+//			.andDo(print())
+//			;
+//		
+//	}
+//    
+//    @Order(3)
+//    @DisplayName("표준 단어 조회")
+//	@Test
+//	void getStdWordById() throws Exception {
+//		
+//        mockMvc.perform(
+//        		get(BASE_URL+"/"+ word_uuid)
+//        		.characterEncoding("UTF-8")
+//                .contentType(MediaTypes.HAL_JSON_VALUE)
+//    			)
+//    			.andDo(print())
+//    			;
+//		
+//	}
+//    
+//    @Order(4)
+//    @DisplayName("표준 단어 수정")
+//    @Transactional
+//	@Test
+//	void putStdWord() throws Exception {
+//    	StdWordEO stdWordEO =  new StdWordEO();
+//    	stdWordEO.setWord_uuid(word_uuid);
+//    	stdWordEO.setWord_dictn_uuid(termn_dictn_uuid);
+//    	stdWordEO.setWord_nm("가동 수정");
+//    	stdWordEO.setWord_eng_nm("Operating 수정");
+//    	stdWordEO.setWord_abbr_nm("OPRTNG 수정");
+//    	stdWordEO.setWord_expl("稼動. 사람이나 기계 따위가 움직여 일함. 또는 기계 따위를 움직여 일하게 함 수정");
+//    	stdWordEO.setWord_clsf_cd("modifier 수정");
+//    	stdWordEO.setFrst_crt_dt("20220704000000");
+//    	stdWordEO.setFrst_crtr("system");
+//    	stdWordEO.setLast_mdfcn_dt("20220704000000");
+//    	stdWordEO.setLast_mdfr("system");
+//    	stdWordEO.setDmnd_dt("20220704000000");
+//    	stdWordEO.setDel_yn(null);
+//		HashMap<String,Object> param = new HashMap<String,Object>();
+//		param.put("word_uuid", stdWordEO.getWord_uuid());
+//		param.put("stdWordEO", stdWordEO);
+//		
+//		mockMvc.perform(
+//				put(BASE_URL)
+//				.characterEncoding("UTF-8")
+//				.contentType(MediaTypes.HAL_JSON_VALUE)
+//				.content(objectMapper.writeValueAsString(param))
+//			)
+//			.andDo(print())
+//			;
+//		
+//	}
+//    
+//    @Order(5)
+//    @DisplayName("표준 단어 삭제")
+//    @Transactional
+//	@Test
+//	void deleteStdWordById() throws Exception {
+//		HashMap<String,Object> param = new HashMap<String,Object>();
+//		param.put("word_uuid", word_uuid);
+//		mockMvc.perform(
+//				delete(BASE_URL)
+//				.characterEncoding("UTF-8")
+//				.contentType(MediaTypes.HAL_JSON_VALUE)
+//				.content(objectMapper.writeValueAsString(param))
+//			)
+//			.andDo(print())
+//			;
+//		
+//	}
+    
 
 }
